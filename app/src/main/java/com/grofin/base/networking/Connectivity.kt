064557f6d02da218b41c978f2989b9051f7e1de6
DeepakSaha1 <deepak.saha@tothenew.com>
2021-06-26 23:25:29 +0530

    network connectivity api check for API below 23

--- app/src/main/java/com/grofin/base/networking/Connectivity.kt
+++ app/src/main/java/com/grofin/base/networking/Connectivity.kt
@@ -2,12 +2,32 @@ package com.grofin.base.networking
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
 
 class Connectivity {
     companion object {
+        @Suppress("DEPRECATION")
         fun isNetworkConnected(context: Context): Boolean {
-            val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
-            return cm!!.activeNetworkInfo != null && cm.activeNetworkInfo!!.isConnected
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val capabilities =
+                    connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                capabilities?.let {
+                    return when {
+                        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                        else -> false
+                    }
+                }
+                return false
+            } else {
+                val networkInfo = connectivityManager.activeNetworkInfo
+                return networkInfo != null && networkInfo.isConnectedOrConnecting
+            }
         }
     }
 }
