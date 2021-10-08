921e56964341c59111c9539aaf2eb089f40714e0
Deepak Saha <animeshdeepak1a@gmail.com>
2021-10-09 00:35:36 +0530

    api end point change

--- app/src/main/java/com/grofin/base/networking/AppEndPoints.kt
+++ app/src/main/java/com/grofin/base/networking/AppEndPoints.kt
@@ -2,7 +2,7 @@ package com.grofin.base.networking
 
 class AppEndPoints {
     companion object {
-        const val BASE_URL = "https://growfin.test/api/"
+        const val BASE_URL = "https://growfinapi.ipranjal.com/api/"
         const val REQ_RES_SINGLE_USER = "api/users/2"
         const val ENDPOINT_REGISTER = "register"
         const val ENDPOINT_LOGIN = "login"
