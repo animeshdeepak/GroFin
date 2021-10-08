f2929eb9f6e7358ad4ef40cebbefb0a8d3b13bee
Deepak Saha <animeshdeepak1a@gmail.com>
2021-10-09 00:35:25 +0530

    ui validation handling

--- app/src/main/java/com/grofin/base/extensions/InputValidation.kt
+++ app/src/main/java/com/grofin/base/extensions/InputValidation.kt
@@ -1,17 +1,33 @@
 package com.grofin.base.extensions
 
+import java.util.regex.Pattern
+
 
 fun String.isMobileValid(): Boolean {
-    // TODO(add valid regex)
-    return !isNullOrEmpty() && !isNullOrBlank() && trim().length == 10
+//    return !isNullOrEmpty() && !isNullOrBlank() && trim().length == 10
+    val pattern = Pattern.compile("(\\d{10})")
+    val matcher = pattern.matcher(this)
+    return matcher.matches()
 }
 
 fun String.isOtpValid(): Boolean {
-    // TODO(add valid regex)
-    return !isNullOrEmpty() && !isNullOrBlank() && trim().length == 6
+//    return !isNullOrEmpty() && !isNullOrBlank() && trim().length == 4
+    val pattern = Pattern.compile("(\\d{4})")
+    val matcher = pattern.matcher(this)
+    return matcher.matches()
 }
 
 fun String.isReferIdValid(): Boolean {
     return !isNullOrEmpty() && !isNullOrBlank() && trim().length == 10
+}
+
+fun String.extractOtp(): String? {
+    //   \d is for a digit
+    //   {} is the number of digits here 4.
+    val pattern = Pattern.compile("(\\d{4})")
+    val matcher = pattern.matcher(this)
+    return if (matcher.find())
+        matcher.group(0)
+    else null
 }
 
